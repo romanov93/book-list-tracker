2767ec77e0be72740007878de58d69646d32dc18
Dmitry Romanov <this.romanovdmitry@gmail.com>
2024-05-10 01:38:59 +0300

    feat(security) add JwtTokenFilter, JwtTokenProvider
    * use JwtTokenFilter in FilterChain
    * JwtTokenProvider can create tokens, update it, validate it and return
      Authentication by access token

---
+++ src/main/java/ru/romanov/booktracker/web/security/JwtTokenFilter.java
@@ -0,0 +1,44 @@
+package ru.romanov.booktracker.web.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+import ru.romanov.booktracker.domain.exception.ResourceNotFoundException;
+
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+public class JwtTokenFilter extends GenericFilterBean {
+
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
+        String bearerToken = ((HttpServletRequest) servletRequest).getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            bearerToken = bearerToken.substring(7);
+        }
+        if (bearerToken != null && jwtTokenProvider.isValid(bearerToken)) {
+            try {
+                Authentication authentication = jwtTokenProvider.getAuthentication(bearerToken);
+                if (authentication != null)
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (ResourceNotFoundException ignored) {
+
+            }
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
---
+++ src/main/java/ru/romanov/booktracker/web/security/JwtTokenProvider.java
@@ -0,0 +1,131 @@
+package ru.romanov.booktracker.web.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import ru.romanov.booktracker.domain.exception.AccessDeniedException;
+import ru.romanov.booktracker.domain.user.Role;
+import ru.romanov.booktracker.domain.user.User;
+import ru.romanov.booktracker.service.interfaces.UserService;
+import ru.romanov.booktracker.service.props.JwtProperties;
+import ru.romanov.booktracker.web.dto.auth.JwtResponse;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    private final JwtProperties jwtProperties;
+
+    private final UserDetailsService userDetailsService;
+
+    private final UserService userService;
+
+    private SecretKey key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes());
+    }
+
+    public String createAccessToken(Long userId, String username, Set<Role> roles) {
+        Claims claims = Jwts.claims()
+                .subject(username)
+                .add("id", userId)
+                .add("roles", resolveRoles(roles))
+                .build();
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + jwtProperties.getAccess());
+        return Jwts.builder()
+                .claims(claims)
+                .issuedAt(now)
+                .expiration(validity)
+                .signWith(key)
+                .compact();
+    }
+
+    public String createRefreshToken(Long userId, String username) {
+        Claims claims = Jwts.claims()
+                .subject(username)
+                .add("id", userId)
+                .build();
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + jwtProperties.getRefresh());
+        return Jwts.builder()
+                .claims(claims)
+                .issuedAt(now)
+                .expiration(validity)
+                .signWith(key)
+                .compact();
+    }
+
+    public JwtResponse refreshUserTokens(String refreshToken) {
+        JwtResponse jwtResponse = new JwtResponse();
+
+        if (!isValid(refreshToken))
+            throw new AccessDeniedException();
+
+        Long userId = Long.valueOf(getId(refreshToken));
+        User user = userService.findById(userId);
+        jwtResponse.setId(userId);
+        jwtResponse.setUsername(user.getUsername());
+        jwtResponse.setAccessToken(createAccessToken(userId, user.getUsername(), user.getRoles()));
+        jwtResponse.setRefreshToken(createRefreshToken(userId, user.getUsername()));
+        return jwtResponse;
+    }
+
+    public boolean isValid(String token) {
+        Jws<Claims> claims = Jwts
+                .parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token);
+        return claims
+                .getPayload()
+                .getExpiration()
+                .after(new Date());
+    }
+
+    public Authentication getAuthentication(String token) {
+        String username = getUsername(token);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    private String getUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    private String getId(String token) {
+        return Jwts.parser()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("id")
+                .toString();
+    }
+    private List<String> resolveRoles(Set<Role> roles) {
+        return roles.stream()
+                .map(Role::name)
+                .toList();
+    }
+}
