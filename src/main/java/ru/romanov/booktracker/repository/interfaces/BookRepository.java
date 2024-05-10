4be068b4309b92e85b8c8e9d67d2bcffb381c01a
Dmitry Romanov <this.romanovdmitry@gmail.com>
2024-05-11 00:24:51 +0300

    feat(repositories) add Mapper annotation on repos
    * add mybatis "@Mapper" annotation on UserRepository and BookRepository
      interfaces

--- src/main/java/ru/romanov/booktracker/repository/interfaces/BookRepository.java
+++ src/main/java/ru/romanov/booktracker/repository/interfaces/BookRepository.java
@@ -1,11 +1,13 @@
 package ru.romanov.booktracker.repository.interfaces;
 
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import ru.romanov.booktracker.domain.book.Book;
 
 import java.util.List;
 
+@Mapper
 public interface BookRepository extends CrudRepository<Book>{
 
     List<Book> findAllByUserId(Long userId);
--- src/main/java/ru/romanov/booktracker/repository/interfaces/UserRepository.java
+++ src/main/java/ru/romanov/booktracker/repository/interfaces/UserRepository.java
@@ -1,11 +1,13 @@
 package ru.romanov.booktracker.repository.interfaces;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import ru.romanov.booktracker.domain.user.Role;
 import ru.romanov.booktracker.domain.user.User;
 
 import java.util.Optional;
 
+@Mapper
 public interface UserRepository extends CrudRepository<User> {
 
     Optional<User> findByUsername(String username);
