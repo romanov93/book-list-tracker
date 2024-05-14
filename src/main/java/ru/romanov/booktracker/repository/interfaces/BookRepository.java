d25c959bf42e9da80a95e22a47092a37f8a59dc7
Dmitry Romanov <this.romanovdmitry@gmail.com>
2024-05-14 17:17:37 +0300

    feat(repository) change jdbc statements to mybatis
    * comment Repository annotation in UserRepoImpl and BookRepoImpl so that
      beans will not be created
    * add Mapper mybatis annotation to UserRepo and BookRepo interfaces

--- src/main/java/ru/romanov/booktracker/repository/interfaces/BookRepository.java
+++ src/main/java/ru/romanov/booktracker/repository/interfaces/BookRepository.java
@@ -1,13 +1,20 @@
 package ru.romanov.booktracker.repository.interfaces;
 
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 import ru.romanov.booktracker.domain.book.Book;
 
 import java.util.List;
+import java.util.Optional;
 
+@Mapper
 public interface BookRepository extends CrudRepository<Book>{
 
     List<Book> findAllByUserId(Long userId);
 
-    void assignToUserById(Long userId, Long bookId);
+    void assignToUserById(@Param("userId") Long userId,
+                          @Param("bookId") Long bookId);
 
 }
--- src/main/java/ru/romanov/booktracker/repository/interfaces/UserRepository.java
+++ src/main/java/ru/romanov/booktracker/repository/interfaces/UserRepository.java
@@ -1,16 +1,21 @@
 package ru.romanov.booktracker.repository.interfaces;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 import ru.romanov.booktracker.domain.user.Role;
 import ru.romanov.booktracker.domain.user.User;
 
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User> {
+@Mapper
+public interface UserRepository extends CrudRepository<User>{
 
     Optional<User> findByUsername(String username);
 
-    void insertUserRole(Long userId, Role role);
+    void insertUserRole(@Param("userId") Long userId,
+                        @Param("role") Role role);
 
     // нужно для проверки доступа юзера к книге
-    boolean isBookOwner(Long userId, Long bookId);
+    boolean isBookOwner(@Param("userId") Long userId, @Param("bookId") Long bookId);
 }
