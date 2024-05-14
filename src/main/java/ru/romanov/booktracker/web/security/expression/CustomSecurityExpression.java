63e157eb0d2c7c36e196ea92dad0019ada6acc3d
Dmitry Romanov <this.romanovdmitry@gmail.com>
2024-05-15 02:30:18 +0300

    feat(security) add CustomSecurityExpression class
    * just Component with boolean methods which should be used for check
      something in PreAuthorized annotation in controllers

---
+++ src/main/java/ru/romanov/booktracker/web/security/expression/CustomSecurityExpression.java
@@ -0,0 +1,47 @@
+package ru.romanov.booktracker.web.security.expression;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import ru.romanov.booktracker.domain.user.Role;
+import ru.romanov.booktracker.service.interfaces.UserService;
+import ru.romanov.booktracker.web.security.JwtEntity;
+
+import static ru.romanov.booktracker.domain.user.Role.ROLE_ADMIN;
+
+@Service("customSecurityExpression")
+@RequiredArgsConstructor
+public class CustomSecurityExpression {
+
+    private final UserService userService;
+
+    public boolean canAccessUser(Long id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        JwtEntity user = (JwtEntity) authentication.getPrincipal();
+        Long userId = user.getId();
+
+        return userId.equals(id) || hasAnyRole(authentication, ROLE_ADMIN);
+    }
+
+    public boolean canAccessBook(Long bookId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        JwtEntity user = (JwtEntity) authentication.getPrincipal();
+        Long userId = user.getId();
+
+        return userService.isBookOwner(userId, bookId);
+    }
+
+    private boolean hasAnyRole(Authentication authentication, Role... roles) {
+        for (Role role : roles) {
+            SimpleGrantedAuthority authority = new SimpleGrantedAuthority(role.name());
+            if (authentication.getAuthorities().contains(authority))
+                return true;
+        }
+        return false;
+    }
+
+}
