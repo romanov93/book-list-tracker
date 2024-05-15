bc1fd44d3857cc35e8b5e02d39998030050a91a2
Dmitry Romanov <this.romanovdmitry@gmail.com>
2024-05-15 17:06:07 +0300

    feat(reddis) add EnableCaching annotation

--- src/main/java/ru/romanov/booktracker/BookTrackerApplication.java
+++ src/main/java/ru/romanov/booktracker/BookTrackerApplication.java
@@ -2,8 +2,10 @@ package ru.romanov.booktracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BookTrackerApplication {
 
     public static void main(String[] args) {
