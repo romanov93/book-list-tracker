1ebb42255f6bca59bb3b268d1e48c178da404a6c
Dmitry Romanov <this.romanovdmitry@gmail.com>
2024-05-27 14:51:58 +0300

    feat(graphql) bind graphql with controllers
    * add mutations and queries to UserController and BookController

--- src/main/java/ru/romanov/booktracker/web/controller/UserController.java
+++ src/main/java/ru/romanov/booktracker/web/controller/UserController.java
@@ -3,6 +3,9 @@ package ru.romanov.booktracker.web.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -32,42 +35,48 @@ public class UserController {
     private final BookMapper bookMapper;
 
     @GetMapping("/{id}")
+    @QueryMapping(name = "userById")
     @Operation(summary = "Get UserDto by id")
     @PreAuthorize("@customSecurityExpression.canAccessUser(#id)")
-    public UserDto getById(@PathVariable Long id) {
+    public UserDto getById(@PathVariable @Argument Long id) {
         User user = userService.findById(id);
         return userMapper.toDto(user);
     }
 
     @PutMapping
+    @MutationMapping(name = "updateUser")
     @Operation(summary = "Update user")
     @PreAuthorize("@customSecurityExpression.canAccessUser(#userDto.id)")
-    public UserDto update(@Validated(OnUpdate.class) @RequestBody UserDto userDto) {
+    public UserDto update(@Validated(OnUpdate.class)
+                          @RequestBody @Argument UserDto userDto) {
         User user = userMapper.toEntity(userDto);
         User updatedUser = userService.update(user);
         return userMapper.toDto(updatedUser);
     }
 
     @DeleteMapping("/{id}")
+    @MutationMapping(name = "deleteUser")
     @Operation(summary = "Delete user by id")
     @PreAuthorize("@customSecurityExpression.canAccessUser(#id)")
-    public void deleteById(@PathVariable Long id) {
+    public void deleteById(@PathVariable @Argument Long id) {
         userService.delete(id);
     }
 
     @GetMapping("/{id}/books")
+    @QueryMapping(name = "booksByUserId")
     @Operation(summary = "Get BookDto list by userId")
     @PreAuthorize("@customSecurityExpression.canAccessUser(#id)")
-    public List<BookDto> getBooksByUserId(@PathVariable Long id) {
+    public List<BookDto> getBooksByUserId(@PathVariable @Argument Long id) {
         List<Book> usersBooks = bookService.getAllByUserId(id);
         return bookMapper.toDto(usersBooks);
     }
 
     @PostMapping("/{id}/books")
+    @MutationMapping(name = "createBook")
     @Operation(summary = "Add book to user")
     @PreAuthorize("@customSecurityExpression.canAccessUser(#userId)")
-    public BookDto createBook(@PathVariable(name = "id") Long userId,
-                              @Validated(OnCreate.class) @RequestBody BookDto bookDto) {
+    public BookDto createBook(@PathVariable(name = "id") @Argument Long userId,
+                              @Validated(OnCreate.class) @RequestBody @Argument BookDto bookDto) {
         Book book = bookMapper.toEntity(bookDto);
         Book createdBook = bookService.create(book, userId);
         return bookMapper.toDto(createdBook);
